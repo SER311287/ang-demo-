c5af7ccb8ff5362974e5a01473c9c640d7e0aaa1
Sergio Telon <sergiovalley2k@gmail.com>
2021-09-05 23:07:52 -0600

    Este es el primer ejercicio de angula usando String interpolation y data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-demo';
+ 
+ // nombre="Leonel";
+
+  // estamos creando un objeto con varias propiedades y que accedemos
+  // otra manera de hacer data banding 
+
+/*
+persona: any={
+  nombre:'Leonel',
+  apellido:'Telon',
+  edad:'35' */
+  /*
+
+  getSuma():number{
+    return 10 + 25 ;
+
+  }*/
+
+  saludo='HOLA MUNDO ';
+
+  urlImg='https://www.metaltoad.com/sites/default/files/styles/large_personal_photo_870x500_/public/2020-05/angularjs-logo-blog-header_0.jpg?itok=CUBxFqam'
+
+
+  numero1 = 0 ;
+  numero2 = 0 ;
+
+  suma(): number {
+
+    return  this.numero1 + this.numero2  
+
+  }
+
+
+
 }
+
+
